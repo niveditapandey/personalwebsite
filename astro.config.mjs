1d13a5934489c302141846c6e6a0947e12b7a015
niveditapandey <nivedita.pandey.wg15@gmail.com>
2026-03-12 18:43:00 +0530

    Updated Astro Config to Vercel Website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://niveditapandey.com',
+  site: 'https://personalwebsite-phuzd4ur4-nivedita-pandeys-projects.vercel.app',
   integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
